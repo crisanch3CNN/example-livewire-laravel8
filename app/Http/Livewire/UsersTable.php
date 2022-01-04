3f4cf3d687342df35b8dd318d04d40e2834f76a4
crisanch <cristian.sanchez@triciclos.cl>
2022-01-04 19:44:53 -0300

    feat(users):  add pagination

--- app/Http/Livewire/UsersTable.php
+++ app/Http/Livewire/UsersTable.php
@@ -4,13 +4,16 @@ namespace App\Http\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersTable extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.users-table', [
-            'users' => User::paginate(10),
+            'users' => User::paginate(5),
         ]);
     }
 }
