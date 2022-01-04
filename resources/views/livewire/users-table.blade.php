6ea09925a76ff0c11d8e1a68d9c932a7fefc42ab
crisanch <cristian.sanchez@triciclos.cl>
2022-01-04 19:15:25 -0300

    refactor(users): delete view users

--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -1,6 +1,20 @@
+
 <div>
-    @foreach($users as $user)
-        <p>{{$user->name}}</p> 
-    @endforeach
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }} desde LiveWire
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            @foreach($users as $user)
+                <p>{{$user->name}}</p> 
+            @endforeach
+            </div>
+        </div>
+    </div>
+
 
 </div>
