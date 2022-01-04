989308f4abc93e97151f9443042fccd1ff06abc4
crisanch <cristian.sanchez@triciclos.cl>
2022-01-04 19:10:58 -0300

    feat(users): create components users-table

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-               @foreach($users as $user)
-                   <p>{{$user->name}}</p> 
-               @endforeach
+
+               <livewire:users-table></livewire:users-table>
+
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Models\User;
+use App\Http\Livewire\UsersTable;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,8 +11,6 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-Route::middleware(['auth:sanctum', 'verified'])->get('/users', function () {
-    return view('users', [
-        'users' => User::paginate(),
-    ]);
-})->name('users');
+Route::middleware(['auth:sanctum', 'verified'])
+    ->get('/users', UsersTable::class)
+    ->name('users');
